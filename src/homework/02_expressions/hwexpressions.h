0b96fdc063d0d1c67b523f8a10bb32c02ec0cf85
TaylorASmith <157528024+TaylorASmith@users.noreply.github.com>
2024-02-07 01:02:53 +0000

    git commit -m "HW 2/6"

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,16 @@
 ﻿//example
+#include "catch.hpp"
 int add_numbers(int num1, int num2);
 
 //write function prototype here
+TEST_CASE("get_sales_tax_amount")
+{
+    REQUIRE get_sales_tax_amount(10) == 0.675;
+    assert get_sales_tax_amount(20) == 1.35;
+}
 
-
-
-
+TEST_CASE("get_tip_amount")
+{
+    assert get_tip_amount(20, 0.15) == 3;
+    assert get_tip_amount(20, 0.2) == 4;
+}
--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -2,9 +2,8 @@
 #include "catch.hpp"
 #include "hwexpressions.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
-}
+TEST_CASE("Verify Test Configuration", "verification") 
+{	REQUIRE(true == true);	}
 
 TEST_CASE("Verify sum_numbers function") 
 {
@@ -12,3 +11,36 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+double get_sales_tax_amount(double meal_amount);
+double get_tip_amount(double meal_amount, double tip_rate);
+
+int main() {
+    double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+    std::cout << "Enter the meal amount: ";
+    std::cin >> meal_amount;
+
+    tax_amount = get_sales_tax_amount(meal_amount);
+
+    std::cout << "Enter the tip rate (in decimal form): ";
+    std::cin >> tip_rate;
+
+    tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+    total = meal_amount + tax_amount + tip_amount;
+
+    std::cout << "Meal Amount:     " << meal_amount << std::endl;
+    std::cout << "Sales Tax:         " << tax_amount << std::endl;
+    std::cout << "Tip Amount:       " << tip_amount << std::endl;
+    std::cout << "Total:                " << total << std::endl;
+
+    return 0;
+}
+
+
+double get_sales_tax_amount(double meal_amount); 
+{	return meal_amount * 0.0675;
+}
+
+double get_tip_amount(double meal_amount, double tip_rate);
+{	return meal_amount * tip_rate;	}
