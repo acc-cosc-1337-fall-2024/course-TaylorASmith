c797e038429d0d2b3aa7f813a1640b469b65130d
TaylorASmith <157528024+TaylorASmith@users.noreply.github.com>
2024-02-08 23:47:43 +0000

    git commit -m "hw 2/8"

--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -38,9 +38,9 @@ int main() {
 }
 
 
-double get_sales_tax_amount(double meal_amount); 
+double get_sales_tax_amount(double meal_amount) 
 {	return meal_amount * 0.0675;
 }
 
-double get_tip_amount(double meal_amount, double tip_rate);
+double get_tip_amount(double meal_amount, double tip_rate)
 {	return meal_amount * tip_rate;	}
