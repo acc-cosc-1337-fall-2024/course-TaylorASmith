eada57a2375c744336c7bc5d9b7f7eef429a52c7
TaylorASmith <157528024+TaylorASmith@users.noreply.github.com>
2024-02-08 23:52:51 +0000

    git commit 2/8 hw 2

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -5,12 +5,12 @@ int add_numbers(int num1, int num2);
 //write function prototype here
 TEST_CASE("get_sales_tax_amount")
 {
-    REQUIRE get_sales_tax_amount(10) == 0.675;
-    assert get_sales_tax_amount(20) == 1.35;
+    get_sales_tax_amount (10) == 0.675;
+    get_sales_tax_amount (20) == 1.35;
 }
 
 TEST_CASE("get_tip_amount")
 {
-    assert get_tip_amount(20, 0.15) == 3;
-    assert get_tip_amount(20, 0.2) == 4;
+    get_tip_amount (20, 0.15) == 3;
+    get_tip_amount (20, 0.2) == 4;
 }
