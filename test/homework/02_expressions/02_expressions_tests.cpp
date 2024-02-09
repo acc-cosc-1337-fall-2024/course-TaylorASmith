e48799970cc081994fb245e8e28d75f2f4aa0a5e
TaylorASmith <157528024+TaylorASmith@users.noreply.github.com>
2024-02-09 00:59:24 +0000

    git commit

--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "hwexpressions.h"
+#include <iostream>
 
 TEST_CASE("Verify Test Configuration", "verification") 
 {	REQUIRE(true == true);	}
@@ -17,22 +18,22 @@ double get_tip_amount(double meal_amount, double tip_rate);
 int main() {
     double meal_amount, tip_rate, tip_amount, tax_amount, total;
 
-    std::cout << "Enter the meal amount: ";
-    std::cin >> meal_amount;
+    cout << "Enter the meal amount: ";
+    cin >> meal_amount;
 
     tax_amount = get_sales_tax_amount(meal_amount);
 
-    std::cout << "Enter the tip rate (in decimal form): ";
-    std::cin >> tip_rate;
+    cout << "Enter the tip rate (in decimal form): ";
+    cin >> tip_rate;
 
     tip_amount = get_tip_amount(meal_amount, tip_rate);
 
     total = meal_amount + tax_amount + tip_amount;
 
-    std::cout << "Meal Amount:     " << meal_amount << std::endl;
-    std::cout << "Sales Tax:         " << tax_amount << std::endl;
-    std::cout << "Tip Amount:       " << tip_amount << std::endl;
-    std::cout << "Total:                " << total << std::endl;
+    cout << "Meal Amount:     " << meal_amount << endl;
+    cout << "Sales Tax:         " << tax_amount << endl;
+    cout << "Tip Amount:       " << tip_amount << endl;
+    cout << "Total:                " << total << endl;
 
     return 0;
 }
