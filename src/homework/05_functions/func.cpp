8f00bce3a21401e06894da462d05f68bde596057
TaylorASmith <157528024+TaylorASmith@users.noreply.github.com>
2024-03-06 00:09:29 +0000

    git commit -m "3/5 Homework"

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,38 @@
 //add include statements
+#include "func.h"
+#include <algorithm>
+
+double get_gc_content(const std::string& dna) {
+ int gc_count = 0;
+ for (char nucleotide : dna) {
+ if (nucleotide == 'G' || nucleotide == 'C') {
+ gc_count++;
+ }
+ }
+ return static_cast<double>(gc_count) / dna.length();
+}
+
+std::string get_reverse_string(std::string dna) {
+ int n = dna.length();
+ for (int i = 0; i < n / 2; i++) {
+ std::swap(dna[i], dna[n - i - 1]);
+ }
+ return dna;
+}
+
+std::string get_dna_complement(std::string dna) {
+ int n = dna.length();
+ for (int i = 0; i < n; i++) {
+ if (dna[i] == 'A')
+ dna[i] = 'T';
+ else if (dna[i] == 'T')
+ dna[i] = 'A';
+ else if (dna[i] == 'C')
+ dna[i] = 'G';
+ else if (dna[i] == 'G')
+ dna[i] = 'C';
+ }
+ return dna;
+}
 
 //add function code here
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,41 @@
+#include <iostream>
+#include "func.h"
+
+using namespace std;
 
 int main() 
 {
+ int choice;
+ string dna;
 
+ do {
+ cout << "Menu\n"
+ << "1- Get GC Content\n"
+ << "2- Get DNA Complement\n"
+ << "3- Exit\n"
+ << "Enter your choice: ";
+ cin >> choice;
+
+ switch (choice) {
+ case 1: {
+ cout << "Enter the DNA string: ";
+ cin >> dna;
+ cout << "GC Content: " << get_gc_content(dna) << endl;
+ break;
+ }
+ case 2: {
+ cout << "Enter the DNA string: ";
+ cin >> dna;
+ cout << "DNA Complement: " << get_dna_complement(dna) << endl;
+ break;
+ }
+ case 3: {
+ cout << "Exiting program...\n";
+ break;
+ }
+ default:
+ cout << "Invalid choice. Please try again." << endl;
+ }
+ } while (choice != 3);
 	return 0;
 }
